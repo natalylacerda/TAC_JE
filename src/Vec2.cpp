a4cf617020f52b567e235ab4e4efcbd13339771d
natalylacerda <nat.lacerda10@gmail.com>
2021-09-15 12:03:18 -0300

    ✨ Creates Vec2

---
+++ src/Vec2.cpp
@@ -0,0 +1,24 @@
+#include "Vec2.h"
+#include <math.h>
+
+using namespace std;
+
+Vec2::Vec2(){
+  this->x = 0;
+  this->y = 0;
+}
+
+Vec2::Vec2(int x, int y){
+  this->x = x;
+  this->y = y;
+}
+
+Vec2 Vec2::GetRotated(float angle){
+  x = this->x * cos(angle) - this->y * sin(angle);
+  y = this->y * cos(angle) - this->x * sin(angle);
+  return Vec2(x, y);
+}
+
+Vec2 operator + (Vec2 const &c1, Vec2 const &c2){
+  return Vec2(c1.x + c2.x, c1.y + c2.y);
+}
