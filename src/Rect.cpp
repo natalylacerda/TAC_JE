57a5454172044557035348dbc8e66129b1040a3c
natalylacerda <nat.lacerda10@gmail.com>
2021-09-15 12:03:53 -0300

    ✨ Creates Rect

---
+++ src/Rect.cpp
@@ -0,0 +1,16 @@
+#include "Rect.h"
+
+using namespace std;
+
+Rect::Rect(){
+  this->x = 0;
+  this->y = 0;
+  this->w = 0;
+  this->h = 0;
+}
+
+Rect::~Rect(){}
+
+bool Rect::Contains(initializer_list<float> mouse){
+  return true;
+}
---
+++ src/Rect.h
@@ -0,0 +1,15 @@
+#ifndef _RECT_H
+#define _RECT_H
+
+#include <initializer_list>
+
+class Rect{
+  
+public:
+  Rect();
+  ~Rect();
+  float x, y, w, h;
+  bool Contains(std::initializer_list<float> mouse);
+};
+
+#endif
